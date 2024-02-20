1c67e8352f4c44be3ec82109d027d1cb4550c5ac
lee.an <lee.an@cognixus.com>
2024-02-20 13:02:09 +0800

    implement firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firestore_demo/firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late var db;
+
+  @override
+  void initState() {
+    super.initState();
+    db = FirebaseFirestore.instance;
+
+    final user = <String, dynamic>{
+      "userAge": 25,
+      "userGender": "Female",
+      "userHeight": 161,
+      "userName": "Florance",
+    };
+
+    db
+        .collection("users")
+        .add(user)
+        .then((DocumentReference doc) => print('DocumentSnapshot added with ID: ${doc.id}'));
+
+    testGetData();
+  }
+
+  void testGetData() async {
+    await db.collection("users").get().then((event) {
+      for (var doc in event.docs) {
+        print("${doc.id} => ${doc.data()}");
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
